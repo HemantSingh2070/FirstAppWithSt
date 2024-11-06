3b2b164988f9dfa218f9039d46bb45e9875d826d
Hemant Singh <hemantsingh2070@gmail.com>
2024-11-06 18:48:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -21,28 +21,6 @@ if uploaded_file is not None:
     st.subheader("Visual Data")
     x_column = st.selectbox("Select X Axis : ",column)
     y_column = st.selectbox("Select Y Axis : ",column)
-
-    button_css = """
-<style>
-    .gradient-button {
-        background: linear-gradient(90deg, #ff7e5f, #feb47b);
-        color: white;
-        border: none;
-        padding: 12px 24px;
-        text-align: center;
-        text-decoration: none;
-        display: inline-block;
-        font-size: 16px;
-        border-radius: 8px;
-        cursor: pointer;
-        transition: background 0.3s ease;
-    }
-    .gradient-button:hover {
-        background: linear-gradient(90deg, #feb47b, #ff7e5f);
-    }
-</style>
-"""
-    st.markdown(button_css, unsafe_allow_html=True)
     if st.button("Generate Plot"):
         st.line_chart(filtered_df.set_index(x_column)[y_column])
 else:
